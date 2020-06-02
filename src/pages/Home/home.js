ba0eeaab7dbf7e68a1da88b25857c57cd137a558
nishanthbs1998 <35337656+nishanthbs1998@users.noreply.github.com>
2020-06-02 23:45:43 +0530

    Updated home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -4,7 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHome,faSearch,faPlusCircle,faRoute,faUserAlt} from '@fortawesome/free-solid-svg-icons';
 import "../../App.css"
 import ted from "../../assets/images/ted.jpg"
-
+//import random from "../../assests/images/ted.jpg"
 const Button=({onClick,children,className=" "})=>{
     return(
         <button type="button"
@@ -46,63 +46,90 @@ class Home extends React.Component{
     return(
         <Container>
             <div className="home-page">
-            <div className="categories">
-                <span style={{width:"25%"}}><CategoryButton   activeCategoryKey={categoryKey} categoryKey="ALL">ALL</CategoryButton></span>
-                <span style={{width:"25%"}}><CategoryButton  activeCategoryKey={categoryKey} categoryKey="ARTICLES" >ARTICLES</CategoryButton></span>
-                <span style={{width:"25%"}}><CategoryButton  activeCategoryKey={categoryKey} categoryKey="PODCASTS" >PODCASTS</CategoryButton></span>
-                <span style={{width:"25%"}}><CategoryButton  activeCategoryKey={categoryKey} categoryKey="VIDEOS" >VIDEOS</CategoryButton></span>
+                <div className="page-title">
+                    <span>Home</span>
+                </div>
+                <div className="banners">
+                    <span ><img src={ted} alt="Suggested content"/></span>
+                </div><br/>
+                <div className="Paths">
+                                
+                    <span style={{width:"50%"}}><Button className="Learning-Path-Title" onClick={()=>{alert("Learning Path")}}>Learning Path 1</Button></span>
+                    <span style={{width:"50%"}}><Button className="Learning-Path-Title" onClick={()=>{alert("Learning Path")}}>Learning Path 2</Button></span>
+                </div>
+                <div className="categories">
+                    <span ><CategoryButton   activeCategoryKey={categoryKey} categoryKey="ALL">ALL</CategoryButton></span>
+                    <span ><CategoryButton  activeCategoryKey={categoryKey} categoryKey="ARTICLES" >ARTICLES</CategoryButton></span>
+                    <span ><CategoryButton  activeCategoryKey={categoryKey} categoryKey="PODCASTS" >PODCASTS</CategoryButton></span>
+                    <span ><CategoryButton  activeCategoryKey={categoryKey} categoryKey="VIDEOS" >VIDEOS</CategoryButton></span>
+                </div>
+                <hr/>
+                <div className="All-contents">
+                    <div className="content">
+                        <div className="content-image">
+                            <table >
+                                <tr style={{textAlign:"center"}}><img src={ted} alt="TED"/></tr>
+                                <tr ><th>Catch the exclusive talk on the most novel web development frameworks for 2020.</th></tr>
+                                <tr >lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.                                                                        
+                                </tr>
+                            </table>
+                        </div>                
+                    </div>
+                    <br/>
+                    <div className="content">
+                        <div className="content-image">
+                            <table >
+                                <tr style={{textAlign:"center"}}><img src={ted} alt="TED"/></tr>
+                                <tr ><th>Catch the exclusive talk on the most novel web development frameworks for 2020.</th></tr>
+                                <tr >lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div className="home-footer">
+                    <span style={{width:"20%"}}><Button className="footer" onClick={()=>alert("Home")}>
+                        <ul style={{listStyleType:"none"}}>
+                            <li style={{fontSize:"18px"}}><FontAwesomeIcon icon={faHome}/></li>
+                            <li style={{fontFamily:"Segoe UI"},{fontSize:'12px'}}>Home</li>
+                        </ul></Button>
+                    </span>
+                    <Button className="footer" onClick={()=>alert("Explore")}>
+                        <span style={{width:"20%"}}>
+                            <ul style={{listStyleType:"none"}}>
+                                <li style={{fontSize:"18px"}}><FontAwesomeIcon icon={faSearch}/></li>
+                                <li style={{fontFamily:"Segoe UI"},{fontSize:'12px'}}>Explore</li>
+                            </ul>
+                        </span>
+                    </Button>
+                    <Button className="footer" onClick={()=>alert("Mind Map")}>
+                        <span style={{width:"20%"}}>
+                            <ul style={{listStyleType:"none"}}>
+                                <li style={{fontSize:"18px"}}><FontAwesomeIcon icon={faPlusCircle}/></li>
+                                <li style={{fontFamily:"Segoe UI"},{fontSize:'12px'}}>Mind Map</li>
+                            </ul>
+                        </span>
+                    </Button>
+                    <Button className="footer" onClick={()=>alert("Learning Path")}>
+                        <span style={{width:"20%"}}>
+                            <ul style={{listStyleType:"none"}}>
+                                <li style={{fontSize:"18px"}}><FontAwesomeIcon icon={faRoute}/></li>
+                                <li style={{fontFamily:"Segoe UI"},{fontSize:'12px'}}>Learning Path</li>
+                            </ul>
+                        </span>
+                    </Button>
+                    <Button className="footer" onClick={()=>alert("Profile")}>
+                        <span style={{width:"20%"}}>
+                            <ul style={{listStyleType:"none"}}>
+                                <li style={{fontSize:"18px"}}><FontAwesomeIcon icon={faUserAlt}/></li>
+                                <li style={{fontFamily:"Segoe UI"},{fontSize:'12px'}}>Profile</li>
+                            </ul>
+                        </span>
+                    </Button>
+                </div>
             </div>
-            <hr/>
-            <div className="content">
-                <ul style={{listStyleType:"none"}}>
-                    <li style={{fontSize:"5x"}}><img src={ted} alt="TED"/></li>
-                    <li style={{fontFamily:"Segoe UI"},{fontSize:'30px'}}>Catch the exclusive talk on the most novel web development frameworks for 2020. Tune in to listen to the interresting insights of industry experts.</li>
-                    <li style={{fontFamily:"Segoe UI"},{fontSize:'20px'}}>lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown typesetter in the 15th century who is
-            thought to .</li>
-                </ul>
-
-            </div>
-            <hr/>
-            <div className="home-footer">
-                <span style={{width:"20%"}}><Button className="footer" onClick={()=>alert("Home")}>
-                    <ul style={{listStyleType:"none"}}>
-                        <li style={{fontSize:"25px"}}><FontAwesomeIcon icon={faHome}/></li>
-                        <li style={{fontFamily:"Segoe UI"},{fontSize:'15px'}}>Home</li>
-                    </ul></Button>
-                </span>
-                <Button className="footer" onClick={()=>alert("Explore")}>
-                <span style={{width:"20%"}}>
-                    <ul style={{listStyleType:"none"}}>
-                        <li style={{fontSize:"25px"}}><FontAwesomeIcon icon={faSearch}/></li>
-                        <li style={{fontFamily:"Segoe UI"},{fontSize:'15px'}}>Explore</li>
-                    </ul>
-                </span></Button>
-                <Button className="footer" onClick={()=>alert("Mind Map")}>
-                <span style={{width:"20%"}}>
-                    <ul style={{listStyleType:"none"}}>
-                        <li style={{fontSize:"25px"}}><FontAwesomeIcon icon={faPlusCircle}/></li>
-                        <li style={{fontFamily:"Segoe UI"},{fontSize:'15px'}}>Mind Map</li>
-                    </ul>
-                </span></Button>
-                <Button className="footer" onClick={()=>alert("Learning Path")}>
-                <span style={{width:"20%"}}>
-                    <ul style={{listStyleType:"none"}}>
-                        <li style={{fontSize:"25px"}}><FontAwesomeIcon icon={faRoute}/></li>
-                        <li style={{fontFamily:"Segoe UI"},{fontSize:'15px'}}>Learning Path</li>
-                    </ul>
-                </span></Button>
-                <Button className="footer" onClick={()=>alert("Profile")}>
-                <span style={{width:"20%"}}>
-                    <ul style={{listStyleType:"none"}}>
-                        <li style={{fontSize:"25px"}}><FontAwesomeIcon icon={faUserAlt}/></li>
-                        <li style={{fontFamily:"Segoe UI"},{fontSize:'15px'}}>Profile</li>
-                    </ul>
-                </span></Button>
-            </div>
-        </div>
-    </Container>    
+        </Container>    
     )
 }
 }
