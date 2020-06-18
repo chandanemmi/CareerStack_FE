2b73682095dc58be01967777311bba45bc988018
chandanemmi <chandanemmi333@gmail.com>
2020-06-18 19:32:05 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,33 +3,19 @@ import Routes from "./Router/index";
 import configureStore from "./store/configureStore";
 import { Provider } from "react-redux";
 import "./scsss/style.scss";
-// import "./App.css";
-// import Demo from "./pages/Explore/Demo";
-import CreateLearningPath from "./pages/Learning-Path/CreateLearningPath";
-//expore
-// import JobRoles from "./pages/Explore/JobRole/JobRoles";
-// import SingleJobRole from "./pages/Explore/JobRole/SingleJobRole";
-// import JobPosting from "./pages/Explore/JobPosting/JobPostings.js";
-// import SingleJobPosting from "./pages/Explore/JobPosting/SingleJobPosting";
-// import Course from "./pages/Explore/Course/Courses.js";
-// import SingleCourse from "./pages/Explore/Course/SingleCourse";
 
 const store = configureStore();
 
-store.subscribe(()=>{
+store.subscribe(() => {
   console.log(store.getState());
-})
+});
 
-const App =(props)=>(
-   <Provider store={store}> 
-  <Routes/>
+const App = (props) => (
+  <Provider store={store}>
+    <Routes />
   </Provider>
-)
+);
 export default App;
-
-
-
-
 
 // function App() {
 //   return (
@@ -52,3 +38,10 @@ export default App;
 //   );
 // }
 
+//expore
+// import JobRoles from "./pages/Explore/JobRole/JobRoles";
+// import SingleJobRole from "./pages/Explore/JobRole/SingleJobRole";
+// import JobPosting from "./pages/Explore/JobPosting/JobPostings.js";
+// import SingleJobPosting from "./pages/Explore/JobPosting/SingleJobPosting";
+// import Course from "./pages/Explore/Course/Courses.js";
+// import SingleCourse from "./pages/Explore/Course/SingleCourse";
